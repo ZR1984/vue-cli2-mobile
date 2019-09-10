d9f28fd8f16b5070c33dde18e03ba27980f0cb23
zhirui <rui.zhi@isinonet.com>
2019-09-10 18:13:28 +0800

    [添加]需要点击确认取消的确认框遮罩

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,65 @@
 <template>
   <div>
-    <LeftCenterRightList :leftCenterRightListArr='leftCenterRightListArr'></LeftCenterRightList>
+    <LeftCenterRightList :leftCenterRightListArr="leftCenterRightListArr"></LeftCenterRightList>
+    <CenterShade
+      :centerShadeContent="centerShadeContent"
+      v-if="isShowCenterShade"
+      @clickCenterShadeOptFuc="clickCenterShadeOptFuc"
+    ></CenterShade>
   </div>
 </template>
 
 <script>
 import LeftCenterRightList from "@/components/leftCenterRightList";
+import CenterShade from "@/components/centerShade";
 
 export default {
   props: [""],
   data() {
     return {
-      leftCenterRightListArr:[
+      leftCenterRightListArr: [
         {
-          id:1,
-          thumb:'/static/images/test.png',
-          name:'123',
-          des:'这是一段测试描述',
-          status:1
+          id: 1,
+          thumb: "/static/images/test.png",
+          name: "123",
+          des: "这是一段测试描述",
+          status: 1
         },
         {
-          id:2,
-          thumb:'/static/images/test.png',
-          name:'223',
-          des:'这是一段测试描述',
-          status:2
-        },
-      ]
+          id: 2,
+          thumb: "/static/images/test.png",
+          name: "223",
+          des: "这是一段测试描述",
+          status: 2
+        }
+      ],
+      centerShadeContent: {
+        title: "同意该笔退款",
+        subTitle: "同意后金额将会退回对方账户",
+        chooseOpt: [
+          { label: "确定", optionFuc: () => true },
+          { label: "取消", optionFuc: () => false }
+        ]
+      },
+      isShowCenterShade: false,
     };
   },
 
   components: {
-    LeftCenterRightList
+    LeftCenterRightList,
+    CenterShade
   },
 
   computed: {},
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    clickCenterShadeOptFuc(data) {
+      this.isShowCenterShade = false;
+      console.log(data);
+    }
+  },
 
   watch: {}
 };
---
+++ src/components/centerShade.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="shade">
+    <div class="inner-box bg_colorFF my_text_center">
+      <p class="color_31 font_size32 m_b30">{{centerShadeContent.title}}</p>
+      <p class="color_9B font_size24">{{centerShadeContent.subTitle}}</p>
+      <ul class="opt-ul flex flex-align-center border_t">
+        <li
+          v-for="(item,index) in centerShadeContent.chooseOpt"
+          :key="index"
+          class="flex-1 font_size28"
+          :class="{'border_r':index!==centerShadeContent.chooseOpt.length-1,'color_81':item.label=='确定'}"
+          @click="clickOptFuc(item.optionFuc)"
+        >{{item.label}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    centerShadeContent: {
+      type: Object,
+      default: ()=>{}
+    }
+  },
+
+  methods: {
+    clickOptFuc(fuc) {
+      this.$emit("clickCenterShadeOptFuc", fuc());
+    }
+  }
+};
+</script>
+<style lang='less' scoped>
+@import url(../assets/css/components/centerShade.less);
+</style>
